00ff4ec117acb6a831dd8d06170a3b52849ca2bc
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-06-29 16:06:20 -0500

    1.1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        vec=line.split('\n')[0]
+        cred=vec.split(',')[2]
+        sys.stdout.write("{}\t1\n".format(cred))
