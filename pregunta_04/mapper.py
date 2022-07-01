85866d6e455340648f70734067ada5e834528b62
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-01 16:58:14 -0500

    1.4

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+if__name__ == "__main__":
+  
+    for line in sys.stdin:
+      letter, date, val = line.strip().split()
+      sys.stdout.writer("{}\t1\n".format(letter))
