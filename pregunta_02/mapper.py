aeeaa78627bf929b50a6051e9452e16016669be5
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-01 15:41:41 -0500

    1.2

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == __main__":
+  for line in sys.stdin:
+    vector= line.split('\n')[0]
+    purpose= vector.split(',')[3]
+    amount= vector.split (',')[4]
+    sys.stdout.write("{}\t{}\n".format(purpose,amount))
