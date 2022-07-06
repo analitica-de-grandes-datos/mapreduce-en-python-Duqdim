5e55af225431eb7867e1e4ec3431c19fa8627f22
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-06 00:24:19 -0500

    5.2

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -7,18 +7,9 @@ import itertools
 
 ##
 
-## Esta funcion reduce los elementos que
-
-## tienen la misma clave
-
-##
-
-
 
 
 if __name__ == '__main__':
-
-
 
 
     curkey = None
@@ -30,15 +21,11 @@ if __name__ == '__main__':
 
     ##
 
-    ## cada linea de texto recibida es una
-
-    ## entrada clave \tabulador valor
+   
 
     ##
 
     for line in sys.stdin:
-
-
 
 
         key, val = line.split("\t") 
@@ -47,51 +34,25 @@ if __name__ == '__main__':
 
 
 
-
         if key == curkey:
 
-            ##
-
-            ## No se ha cambiado de clave. Aca se
-
-            ## acumulan los valores para la misma clave.
-
-            ##
 
             total += val 
 
         else:
 
-            ##
-
-            ## Se cambio de clave. Se reinicia el
-
-            ## acumulador.
-
-            ##
-
             if curkey is not None:
 
                 ##
 
-                ## una vez se han reducido todos los elementos
-
-                ## con la misma clave se imprime el resultado en
-
-                ## el flujo de salida
-
-                ##
+     
 
                 sys.stdout.write("{}\t{}\n".format(curkey, total))
-
-
 
 
             curkey = key
 
             total = val
-
-
 
 
     sys.stdout.write("{}\t{}\n".format(curkey, total))
