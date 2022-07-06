dee3916888f78535605835642c28afadafc88622
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-06 00:33:00 -0500

    4.2

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -2,11 +2,4 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-if __name__ == "__main__":
-  
-  
-    for line in sys.stdin:
-      l= line.split(' ')[0]
-      f= line.split(' ')[1]
-      nu= int(line.split(' ')[2])
-      sys.stdout.write("{}\t{}\t{}\n".format(l,f,n))
+
