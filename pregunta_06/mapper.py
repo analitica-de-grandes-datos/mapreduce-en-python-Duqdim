2dff0fdf4284a86880eb5a987945e69dc0e34676
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-06 00:30:46 -0500

    4.2

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -5,9 +5,9 @@
 import sys
 if __name__ == "__main__":
     tab = [line.replace("\n", "") for line in sys.stdin]
-    tab = [line.split("   ") for line in tabla]
-    colum0 = [tabla[i][0] for i in range(len(tabla))]
-    colum2 = [tabla[i][2] for i in range(len(tabla))]
+    tab = [line.split("   ") for line in tab]
+    colum0 = [tab[i][0] for i in range(len(tab))]
+    colum2 = [tab[i][2] for i in range(len(tab))]
 
     for line in range(len(tabla)):
         sys.stdout.write("{}\t{}\n".format(colum0[line],colum2[line]))
