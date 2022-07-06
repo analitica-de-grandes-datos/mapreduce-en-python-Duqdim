181e42cfea0a33ba8aec77f178bc45288b671fb4
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 21:07:21 -0500

    1.5

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -2,9 +2,11 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-def purpose_amount(x):
-    return x[2]
 
-for line in sys.stdin:
-    result = line.split(',')
-    print(purpose_amount(result))
+if __name__ == "__main__":
+    tabla = [line.replace("\n", "") for line in sys.stdin]
+    tabla = [line.split(",") for line in tabla]
+    col2 = [tabla[i][2] for i in range(len(tabla))]
+
+    for line in col2:
+        sys.stdout.write("{}\t1\n".format(line))
