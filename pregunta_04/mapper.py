1af644aaa7c5e40c698d2213cb1c7cef1bb48e12
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 22:57:35 -0500

    3.2

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,9 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 import sys
+if __name__ == "__main__":
+    tab = [line.replace("\n", "") for line in sys.stdin]
+    tab = [line.split("   ") for line in tabla]
+    colum0 = [tabla[i][0] for i in range(len(tabla))]
 
-if__name__ == "__main__":
-  
-    for line in sys.stdin:
-      letter, date, val = line.strip().split()
-      sys.stdout.writer("{}\t1\n".format(letter))
+    for line in colum0:
+        sys.stdout.write("{}\t1\n".format(line))
