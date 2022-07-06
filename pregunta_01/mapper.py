52e0c3b667555855dc569e11936fadabac502282
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 21:42:51 -0500

    1.6

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -2,11 +2,10 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-
 if __name__ == "__main__":
     tabla = [line.replace("\n", "") for line in sys.stdin]
     tabla = [line.split(",") for line in tabla]
-    col2 = [tabla[i][2] for i in range(len(tabla))]
+    col_2 = [tabla[i][2] for i in range(len(tabla))]
 
-    for line in col2:
+    for line in col_2:
         sys.stdout.write("{}\t1\n".format(line))
