84751955a20e6981ed6ec7e3f8dda245024f305f
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 22:24:14 -0500

    3.2

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -3,41 +3,24 @@
 #
 import sys
 
-elements = {}
+from collections import defaultdict
+import operator
 
-def clear_spaces(x):
+if __name__ == '__main__':
 
-    x = x.replace("\n", "")
+    dicciona = defaultdict(list)
 
-    x = x.replace("\r", "")
+    for line in sys.stdin:
 
-    return x
+        numero, val = line.replace("\n","").strip().split("	")
+        val = val.split(",")
+        numero = int(numero)
+        for elemento in val:
+            dicciona[elemento].append(numero)
 
+    dicciona1 = sorted(diccio.items(), key = operator.itemgetter(0))
 
-def set_bigger_smaller_amount(elements, actual_element):
-
-    element_array = actual_element.split("*")
-
-    if element_array[0] in elements:
-
-        elements[element_array[0]] = elements.get(element_array[0]) +  [int(clear_spaces(element_array[1]))]
-
-    else:
-
-        elements[element_array[0]] = [int(clear_spaces(element_array[1]))]
-
-    return elements
-
-
-for line in sys.stdin:
-
-    set_bigger_smaller_amount(elements, line)
-
-
-
-
-for element, array in  elements.items():
-
-    array = ','.join(str(v) for v in sorted(array))
-
-    print(element + "    " + array)
+    for clave, valor in dicciona1:
+        num = sorted(valor, key = int)
+        val = ",".join(map(str, num))
+        sys.stdout.write("{}	{}\n".format(clave, val))
