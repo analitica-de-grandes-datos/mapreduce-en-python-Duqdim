69b54836b4a0ad9b8cf38e0c3445bde114c40460
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 22:08:20 -0500

    3.1

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,10 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-import sys 
-if __name__== "__main__":
-  
-  for line in sys.stdin:
-    vector= line.split('\n')[0]
-    datos=vector.split(',')
-    sys.stdout.write("{},{}\n".format(datos[0],datos[1]))
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+
+    
+        lista = line.split(",")
+            
+           
+        sys.stdout.write("{},{}\n".format(lista[1].strip(),lista[0]))
