b394ca9dbd039601a3571db46e2d2cb0e9c01d8b
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 22:25:43 -0500

    3.2

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -3,30 +3,13 @@
 #
 import sys
 
+import sys
+if __name__ == "__main__":
+    tab = [line.replace("\n", "") for line in sys.stdin]
+    tab = [line.split("   ") for line in tabla]
+    colum0 = [tabla[i][0] for i in range(len(tabla))]
+    colum1 = [tabla[i][1] for i in range(len(tabla))]
+    colum2 = [tabla[i][2] for i in range(len(tabla))]
 
-def clear_spaces(x):
-
-    x = x.replace("\n", "")
-
-    x = x.replace("\r", "")
-
-    x = x.replace("\t", "")
-
-    return x
-
-
-def purpose_amount(x):
-
-    return   clear_spaces(x[2]) + ";"+  clear_spaces(x[0])+ ";" + clear_spaces(x[1])  
-
-    #return   int(clear_spaces(x[2]))
-
-
-
-for line in sys.stdin:
-
-    line = line.replace("'","")
-
-    result = line.split('   ')
-
-    print(purpose_amount(result))
+    for line in range(len(tab)):
+        sys.stdout.write("{},{},{}\n".format(colum0[line], colum1[line], colum2[line]))
