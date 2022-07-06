167d774b65b0ce4320ab12beed6efab0ef589ab4
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-06 00:21:54 -0500

    4.2

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -2,9 +2,12 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 
 import sys
-if__name__ == "__main__":
-      
-      for line in sys.stdin:
-        letter, date, val =line.strip().split()
-        month=date.split('-')[1]
-        sys.stdout.write("{}\t1\n".format(month))
+if __name__ == "__main__":
+    tab = [line.replace("\n", "") for line in sys.stdin]
+    tab = [line.split("   ") for line in tab]
+    colum = [tabla[i][1] for i in range(len(tab))]
+    colum = [line.split("-") for line in colum]
+    colum1 = [colum[i][1] for i in range(len(colum))]
+
+    for line in colum1:
+        sys.stdout.write("{}\t1\n".format(line))
