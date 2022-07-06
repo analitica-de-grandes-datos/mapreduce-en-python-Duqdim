20c1435c0e27599310c12f805d69ef546f0da529
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 20:26:04 -0500

    1.3

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -2,8 +2,9 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-if __name__ == "__main__":
-    for line in sys.stdin:
-        vec=line.split('\n')[0]
-        cred=vec.split(',')[2]
-        sys.stdout.write("{}\t1\n".format(cred))
+def purpose_amount(x):
+    return x[2]
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(purpose_amount(result))
