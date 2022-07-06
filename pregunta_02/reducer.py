2b64de33d8a0b28a3c9ce2ca9ee2aa1cc46f8c69
Duqdim <102342584+Duqdim@users.noreply.github.com>
2022-07-05 22:02:58 -0500

    2.2

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -2,25 +2,30 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
-import intertools
 
-if __name__ == '__main_':
-  courkey= None
-  to = 0
-  
-  for line in sys.stdin:
-    key,val= line.split("\t")
-    val = int (val)
-    
-    if key == curkey:
-      if(val>total):
-        to=val
-        
-    else:
-      if curkey is not None:
-        sys.stdout.write("{}\t{}\n"-format(curkey,to))
-        
-       corkey = key
-       to = val
-        
-  sys.stdout.write("{}\t{}\n".format(curkey,to))
+if __name__ == '__main__':
+
+    curkey = None
+    contador = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == curkey:
+            if contador >= val:
+                contador = contador
+            else:
+                contador = val
+        else:
+
+            if curkey is not None:
+
+                sys.stdout.write("{}\t{}\n".format(curkey, contador))
+                contador = 0
+
+            curkey = key
+            contador = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, contador))
